3365915fc20538ed1447457e1272bbc0fff52732
Elliott K <emkling@umich.edu>
2022-04-09 14:48:31 -0400

    added input form

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className='flex justify-center items-center bg-gray-400 my-2 '>
+      <div className='animate-spin rounded-full h-20 w-20 border-b-2 border-red-700 '>
+        </div> </div>
   )
 }
 
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,16 +2,34 @@ import React from 'react'
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
+import Loader from './Loader';
 
 const Welcome = () => {
+  const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+
+
+  const handleSubmit = (e) => {
+    
+  }
+
   return (
     <div className='h-screen w-full flex flex-col'>
       <div className='w-full justify-center flex p-2'>
         <h1 className='text-2xl sm:text-6xl mt-56 text-[#c85d5d] text-gradient py-1'>Send Ethereum</h1>
       </div>
 
-      <div className='flex justify-center'> 
-      <div className=' p-3 rounded-xl h-[200px] sm:w-80 w-full my-5 eth-card white-glassmorphism'>
+
+      <div className='flex justify-center py-4'> 
+      <div className=' p-3 rounded-xl h-[200px] w-80  my-5 eth-card white-glassmorphism'>
       <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -30,15 +48,26 @@ const Welcome = () => {
             </div>
         </div>
       </div>
-        <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-
-
-        </div>
       
+      <div className='flex justify-center py-4'>
+        <div className='p-6 w-80 sm:w-96 flex flex-col justify-start items-center blue-glassmorphism'>
+        <Input placeholder="Address To" name="addressTo" type="text"  />
+        <Input placeholder="Amount (ETH)" name="amount" type="number"  />
+        <Input placeholder="Keyword (Gif)" name="keyword" type="text" />
+        <Input placeholder="Enter Message" name="message" type="text"/>
 
-      <div className='justify-center flex p-4'>
-      <a className="border-2 text-center border-[#5d89ee] hover:text-[#c85d5d]  mx-4 py-2 px-7 rounded-full cursor-pointer text-[#FFF] hover:bg-[#2546bd]"> Connect Wallet </a>
+          {false ? ( 
+            <Loader />
+          ) : (
+            <div className='py-4'>
+          
+            <button type='button' className='border-2 text-center border-[#5d89ee] hover:text-[#c85d5d]  mx-4 py-2 px-7 rounded-full cursor-pointer text-[#FFF] hover:bg-[#2546bd]"' onClick={handleSubmit}>Submit </button>
+           </div>)}
+        </div>
+
       </div>
+
+      
 
       </div>
   )

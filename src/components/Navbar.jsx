4b37bc7dd0978b15e5131d0ad98878653d054adb
Elliott K <emkling@umich.edu>
2022-04-09 14:19:11 -0400

    Added card and styles to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,18 +16,19 @@ const Navbar = () => {
   return (
     <nav className="w-full flex justify-between items-center h-[45px] sm:h-[60px]">
       <div className=" flex-initial pl-12 justify-center items-center">
-        <img src={logo} alt="logo" className="w-[170px] -mt-[25px] sm:w-[250px] absolute sm:h-[100px] sm:-mt-[40px] cursor-pointer" />
+        <img src={logo} alt="logo" className="w-[170px] -mt-[25px] sm:w-[250px] absolute sm:h-[100px] sm:-mt-[30px] cursor-pointer" />
       </div>
-      <ul className="text-white w-full lg:flex hidden list-none flex-row mt-[20px] justify-end gap-12 pr-12 items-center flex-initial">
-        <a className="mx-4 cursor-pointer"> <RiMoneyDollarCircleLine className="inline-flex"/> Transactions </a>
-        <a className="mx-4 cursor-pointer"> <AiOutlineDashboard color="#FFFFFF" className="inline-flex "/> Dashboard </a>
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
+      <ul className="text-white w-full lg:flex hidden list-none flex-row mt-[40px] justify-end gap-12 pr-12 items-center flex-initial">
+        <a className="mx-4 cursor-pointer hover:text-[#c85d5d] text-gradient3"> <RiMoneyDollarCircleLine className="inline-flex -mt-[2px]"/> Transactions </a>
+        <a className="mx-4 cursor-pointer  hover:text-[#c85d5d] bg-[#5d89ee]  text-gradient3"> <AiOutlineDashboard  className="inline-flex -mt-[2px]"/> Dashboard </a>
+        <li className=" hover:bg-[#2546bd] border-2 border-[#5d89ee] py-2 px-7 mx-4 rounded-full cursor-pointer">
+          <a className=" text-gradient3"> Connect Wallet</a>
+          
         </li>
       </ul>
       <div className="flex relative">
         {!toggleMenu && (
-          <HiMenuAlt4 fontSize={28} className="text-white lg:hidden cursor-pointer mt-[20px] sm:mt-[20px] mr-12" onClick={() => setToggleMenu(true)} />
+          <HiMenuAlt4 fontSize={28} className="text-white lg:hidden cursor-pointer mt-[20px] sm:mt-[10px] mr-12" onClick={() => setToggleMenu(true)} />
         )}
         {toggleMenu && (
           <AiOutlineClose fontSize={28} className="text-white lg:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div className='h-screen w-full flex flex-col'>
+      <div className='w-full justify-center flex p-2'>
+        <h1 className='text-2xl sm:text-6xl mt-56 text-[#c85d5d] text-gradient py-1'>Send Ethereum</h1>
+      </div>
+
+      <div className='flex justify-center'> 
+      <div className=' p-3 rounded-xl h-[200px] sm:w-80 w-full my-5 eth-card white-glassmorphism'>
+      <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm text-gradient">
+                  address
+                </p>
+                <p className="text-white font-semibold text-lg mt-1 text-gradient">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+        </div>
+      </div>
+        <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+
+
+        </div>
+      
+
+      <div className='justify-center flex p-4'>
+      <a className="border-2 text-center border-[#5d89ee] hover:text-[#c85d5d]  mx-4 py-2 px-7 rounded-full cursor-pointer text-[#FFF] hover:bg-[#2546bd]"> Connect Wallet </a>
+      </div>
+
+      </div>
   )
 }
 

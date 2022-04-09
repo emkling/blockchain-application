d8a070a23862b2d142be38ba65032b2e2af6656f
Elliott K <emkling@umich.edu>
2022-04-09 10:08:20 -0400

    Deleted extra default files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Navbar, Footer, Loader, Transactions, Welcome } from './components'
+import {HiMenuAlt4} from 'react-icons/hi'
+import {AiOUtlineClose} from 'react-icons/ai'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='min-h-screen'>
+        <div className='gradient-bg-welcome'>
+            <Navbar />
+            <Welcome />
+        </div>
+        
+        
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='w-full flex justify-between md:justify-center'>
+
+
+    </nav>
+  )
+}
+
+export default Navbar
